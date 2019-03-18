a866cc71483b0cabda3edd815f82dfffe46d2046
Tobias Gerstenberg <tobiace@gmail.com>
2019-03-17 17:27:27 -0700

    added travis

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,16 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "gerstenberg@stanford.edu"
+git config --global user.name "Tobias Gerstenberg"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
 cd book-output
+git rm -rf *
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m"Update the book"
 git push -q origin gh-pages
