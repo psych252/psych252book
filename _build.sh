1c03f2652bf1763c6802c7bb86826ce6de864922
Tobias Gerstenberg <tobiace@gmail.com>
2019-03-17 19:55:03 -0700

    updated packages in description

--- _build.sh
+++ _build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-Rscript -e "bookdown::render_book(input = './index.Rmd', output_format = 'bookdown::gitbook', new_session = TRUE)"
+Rscript -e "bookdown::render_book(input = './index.Rmd', output_format = 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book(input = './index.Rmd', output_format = 'bookdown::pdf_book', new_session = TRUE)"
